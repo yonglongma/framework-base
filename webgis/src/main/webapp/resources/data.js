f6b73848eb15eea4dffca1cbb4bd5fdc2dcc5e61
yonglongma <merleni123>
2017-09-07 18:23:45 +0800

    webgis相关demo(点标记，点聚合，热力图，点击定位，自适应显示标记，popup等

---
+++ webgis/src/main/webapp/resources/data.js
@@ -0,0 +1,12 @@
+var dataList = [
+    {name:"数据1",latitude:"31.17462",longitude:"121.65178",icon:"resources/images/icon1.png",description:"我是数据1描述1111111111111"},
+    {name:"数据2",latitude:"31.17359",longitude:"121.69436",icon:"resources/images/icon2.png",description:"我是数据2描述1111111111111"},
+    {name:"数据3",latitude:"31.17374",longitude:"121.61573",icon:"resources/images/icon3.png",description:"我是数据3描述1111111111111"},
+    {name:"数据4",latitude:"31.16845",longitude:"121.6856",icon:"resources/images/icon2.png",description:"我是数据4描述1111111111111"},
+    {name:"数据5",latitude:"31.1567",longitude:"121.55239",icon:"resources/images/icon1.png",description:"我是数据5描述1111111111111"},
+    {name:"数据6",latitude:"31.13555",longitude:"121.57642",icon:"resources/images/icon1.png",description:"我是数据6描述1111111111111"},
+    {name:"数据7",latitude:"31.30437",longitude:"121.35635",icon:"resources/images/icon2.png",description:"我是数据7描述1111111111111"},
+    {name:"数据8",latitude:"31.07058",longitude:"121.45454",icon:"resources/images/icon3.png",description:"我是数据8描述1111111111111"},
+    {name:"数据9",latitude:"31.14701",longitude:"121.6008",icon:"resources/images/icon2.png",description:"我是数据9描述1111111111111"},
+    {name:"数据10",latitude:"31.06764",longitude:"121.72234",icon:"resources/images/icon1.png",description:"我是数据10描述1111111111111"}
+];
